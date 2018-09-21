cd27f732a7e6a6790328c85370b22c5387701590
Никита Поносов <ponosov@databriz.ru>
2018-09-21 17:28:03 +0500

    Описал 3 действия(есть, спать, отдыхать)

--- src/main/java/game/engine/actions/Impl/EatActionImpl.java
+++ src/main/java/game/engine/actions/Impl/EatActionImpl.java
@@ -1,6 +1,63 @@
 package game.engine.actions.Impl;
 
+import game.consts.ActionConst;
+import game.consts.PlantTypeConst;
+import game.data.enums.ActionEnum;
+import game.data.enums.PlantEnum;
+import game.engine.Game;
 import game.engine.actions.EatAction;
+import game.engine.mechanics.Impl.CostMechanic;
+import game.engine.objects.GameMapCell;
+import game.engine.objects.units.GenericUnit;
+import game.engine.objects.units.Rabbit;
+import game.engine.tactor.Tactor;
 
 public class EatActionImpl extends GenericGoapActionImpl implements EatAction {
+    @Override
+    public void doAction(Game game, GenericUnit actor) {
+        if (actor instanceof Rabbit) {
+            Rabbit rabbit = (Rabbit) actor;
+            GameMapCell cell = game.map.getCell(rabbit.x, rabbit.y);
+            Tactor tactor = game.tactor;
+            synchronized (cell){
+                if (cell.plant != PlantTypeConst.NO_PLANT) {
+                    rabbit.setFat(rabbit.getFat() + PlantEnum.getByNumberOfTitle(cell.plant).plantCost);
+                    cell.plant = PlantTypeConst.NO_PLANT;
+                    cell.eatedAtTime = tactor.getInnerTime();
+                    CostMechanic.setFromAction(rabbit, ActionEnum.EAT);
+                    rabbit.setLastAction(ActionEnum.EAT);
+                    rabbit.eatedGrass++;
+                    rabbit.setCurrentActionPicture(ActionConst.EAT);
+                }
+            }
+        } else {
+            super.doAction(game, actor);
+        }
+    }
+
+    @Override
+    public Double calcCost(Game game, GenericUnit actor) {
+        if (actor instanceof Rabbit){
+            Rabbit rabbit = (Rabbit) actor;
+            GameMapCell cell = game.map.getCell(rabbit.x, rabbit.y);
+            if (rabbit.getMaxFat() - rabbit.getFat() >= PlantEnum.getByNumberOfTitle(cell.plant).plantCost) {
+                return 0.0 - PlantEnum.getByNumberOfTitle(cell.plant).plantCost;
+            } else {
+                return 100.0;
+            }
+        } else {
+            return super.calcCost(game, actor);
+        }
+    }
+
+    @Override
+    public boolean isAvailable(Game game, GenericUnit actor) {
+        if (actor instanceof Rabbit){
+            Rabbit rabbit = (Rabbit) actor;
+            GameMapCell cell = game.map.getCell(rabbit.x, rabbit.y);
+            return cell.plant != PlantTypeConst.NO_PLANT;
+        } else {
+            return super.isAvailable(game, actor);
+        }
+    }
 }
--- src/main/java/game/engine/actions/Impl/SleepActionImpl.java
+++ src/main/java/game/engine/actions/Impl/SleepActionImpl.java
@@ -1,6 +1,66 @@
 package game.engine.actions.Impl;
 
+import game.consts.ActionConst;
+import game.consts.AnimalStatConst;
+import game.data.enums.ActionEnum;
+import game.engine.Game;
 import game.engine.actions.SleepAction;
+import game.engine.mechanics.Impl.CostMechanic;
+import game.engine.mechanics.Impl.MotivationMechanic;
+import game.engine.objects.units.GenericUnit;
+import game.engine.objects.units.Rabbit;
+
+import java.util.Random;
 
 public class SleepActionImpl extends GenericGoapActionImpl implements SleepAction {
+
+    @Override
+    public void doAction(Game game, GenericUnit actor) {
+        if (actor instanceof Rabbit) {
+            Rabbit rabbit = (Rabbit) actor;
+            CostMechanic.setFromAction(rabbit, ActionEnum.SLEEP);
+            rabbit.setLastAction(ActionEnum.SLEEP);
+            rabbit.setCurrentActionPicture(ActionConst.SLEEP);
+        } else {
+            super.doAction(game, actor);
+        }
+    }
+
+    @Override
+    public Double calcCost(Game game, GenericUnit actor) {
+        if (actor instanceof Rabbit) {
+            Rabbit rabbit = (Rabbit) actor;
+            //Безусловный сон при максимальной сонливости
+            if (rabbit.getNeedSleeping() == AnimalStatConst.ABSOLUTELY_NEED_SLEEP) return -100.0;
+
+            //Большая вероятность уснуть если всё нормально и большая сонливость
+            if (rabbit.getNeedSleeping() > AnimalStatConst.ALMOSTLY_NEED_SLEEP) {
+                Random random = new Random();
+                int seed = random.nextInt(AnimalStatConst.BASE_RANDOM_SLEEPING);
+                if (seed < AnimalStatConst.RANDOM_START_SLEEPING_PERCENT) return -5.0;
+            }
+
+            //Продолжение сна если всё нормально и сонливость не низкая
+            if (rabbit.getNeedSleeping() > AnimalStatConst.CONTINUE_NEED_SLEEP && rabbit.isLastSleep()) {
+                Random random = new Random();
+                int seed = random.nextInt(AnimalStatConst.BASE_RANDOM_SLEEPING);
+                if (seed < AnimalStatConst.RANDOM_SLEEPING_PERCENT) return -5.0;
+            }
+
+            // В любом другом случае не спим
+            return 90.0;
+        } else {
+            return super.calcCost(game, actor);
+        }
+    }
+
+    @Override
+    public boolean isAvailable(Game game, GenericUnit actor) {
+        if (actor instanceof Rabbit) {
+            Rabbit rabbit = (Rabbit) actor;
+            return ! MotivationMechanic.hasDanger(rabbit, game);
+        } else {
+            return super.isAvailable(game, actor);
+        }
+    }
 }

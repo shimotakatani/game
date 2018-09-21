7aea9b4cbb8d593ed81d253e0f293a37a502da16
Никита Поносов <ponosov@databriz.ru>
2018-09-21 16:38:48 +0500

    Минимальные дженерики

--- src/main/java/game/engine/actions/Impl/GenericActionImpl.java
+++ src/main/java/game/engine/actions/Impl/GenericActionImpl.java
@@ -1,10 +1,18 @@
 package game.engine.actions.Impl;
 
+import game.engine.Game;
+import game.engine.actions.GenericAction;
+import game.engine.objects.units.GenericUnit;
+
 /**
  * create time 22.02.2018
  *
  * Реализация общего действия
  * @author nponosov
  */
-public class GenericActionImpl {
+public abstract class GenericActionImpl implements GenericAction {
+
+    public void doAction(Game game, GenericUnit actor) {
+        //Писать выполняемые действия сюда
+    }
 }
